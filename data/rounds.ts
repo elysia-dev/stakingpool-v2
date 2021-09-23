7d5a1d404980d9abc8ff71c5974d266fd3519456
jaebok <jaebok.lee@modori.io>
2021-09-23 11:57:57 +0900

    add tasks & data

--- data/rounds.ts
+++ data/rounds.ts
@@ -5,9 +5,9 @@ import { SECONDSPERDAY} from '../test/utils/constants'
 export const third: InitRoundData = {
   rewardPerSecond: ethers.utils.parseEther('1'),
   year: 2021,
-  month: 11,
-  day: 10,
-  hour: 9,
+  month: 9,
+  day: 25,
+  hour: 10,
   minute: 0,
-  duration: ethers.BigNumber.from(30).mul(SECONDSPERDAY).toNumber(),
+  duration: ethers.BigNumber.from(40).mul(SECONDSPERDAY).toNumber(),
 };
